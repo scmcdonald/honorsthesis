9b9109d3891702973875fc66d4288d78925b660d
Sarah <scm81106@marymount.edu>
2019-10-16 09:51:54 -0400

    more ipeds work

--- ipeds_data.R
+++ ipeds_data.R
@@ -36,7 +36,7 @@ extra <- ""
 capital_extra <- ""
 
 # getIPEDSData(year, survey_file, capital_survey_file, extra, capital_extra)
-for (year in 1990:2017){
+for (year in 2000:2017){
   try(
     assign(paste(survey_file, year, extra, sep = ""), 
       getIPEDSData(year, survey_file, capital_survey_file, extra, capital_extra)))
@@ -120,12 +120,15 @@ variables[nrow(variables) + 1,] = list(Table= factor("HD"),Variable.Number= 9999
 HD_var <- variables[variables$Table == "HD", 3]
 
 # Merge and and only use the columns in our variable list
+
+HD <- lapply(HD, function(x) x[(names(x)) %in% HD_var])
 HD_df <- do.call("smartbind", HD)
-HD_df <- HD_df[ ,colnames(HD_df) %in% HD_var]
+
+
 
 # Will need to come up with CBSA and Lat/Long for missing values in earlier years
 
-
+HD_df$YEAR[HD_df$YEAR == "98"] <- "1998"
 
 ################EFFY#######################3
 survey_file <- "effy"
@@ -176,7 +179,12 @@ colnames(ef2001d1)[colnames(ef2001d1)=="FYRACE17"] <- "EFYTOTLT"
 #Total Men
 colnames(ef2001d1)[colnames(ef2001d1)=="FYRACE15"] <- "EFYTOTLM"
 
-#Total Hispanic - I can add total hispanic women and total hispanic men to get grand total hispanic 
+#total Hispanic men
+colnames(ef2001d1)[colnames(ef2001d1)=="FYRACE09"] <- "EFYHISPM"
+colnames(ef2001d1)[colnames(ef2001d1)=="FYRACE10"] <- "EFYHISPW"
+
+#total hispanic men and women
+ef2001d1$EFYHISPT <- ef2001d1$EFYHISPW + ef2001d1$EFYHISPM
 
 #prior to 2001, data is divided by undergraduates and graduates (rather by race/gender)
 
@@ -189,8 +197,9 @@ variables[nrow(variables) + 1,] = list(Table= factor("EFFY"),Variable.Number= 99
 EFFY_var <- variables[variables$Table == "EFFY", 3]
 
 # Merge and and only use the columns in our variable list
+
+EFFY <- lapply(EFFY, function(x) x[(names(x)) %in% EFFY_var])
 EFFY_df <- do.call("smartbind", EFFY)
-EFFY_df <- EFFY_df[ ,colnames(EFFY_df) %in% EFFY_var]
 
 ####################################efa###########################################
 
@@ -204,6 +213,87 @@ for (year in 2000:2017){
          getIPEDSData(year, survey_file, capital_survey_file, extra, capital_extra))
 }
 
+#fix 2000
+#2000 total men
+colnames(ef2000a)[colnames(ef2000a)=="EFRACE15"] <- "EFTOTLM"
+#2000 total women
+colnames(ef2000a)[colnames(ef2000a)=="EFRACE16"] <- "EFTOTLW"
+#total men and women
+ef2000a$EFTOTLT <- ef2000a$EFTOTLM + ef2000a$EFTOTLW
+
+
+#2000 total Hispanic men
+colnames(ef2000a)[colnames(ef2000a)=="EFRACE09"] <- "EFHISPM"
+colnames(ef2000a)[colnames(ef2000a)=="EFRACE10"] <- "EFHISPW"
+
+#total hispanic men and women
+ef2000a$EFHISPT <- ef2000a$EFHISPW + ef2000a$EFHISPM
+
+
+#fix 2001
+#2001 total men
+colnames(ef2001a)[colnames(ef2001a)=="EFRACE15"] <- "EFTOTLM"
+#2001 total women
+colnames(ef2001a)[colnames(ef2001a)=="EFRACE16"] <- "EFTOTLW"
+#total men and women
+ef2001a$EFTOTLT <- ef2001a$EFTOTLM + ef2001a$EFTOTLW
+
+#2001 total Hispanic men
+colnames(ef2001a)[colnames(ef2001a)=="EFRACE09"] <- "EFHISPM"
+colnames(ef2001a)[colnames(ef2001a)=="EFRACE10"] <- "EFHISPW"
+
+#total hispanic men and women
+ef2001a$EFHISPT <- ef2001a$EFHISPW + ef2001a$EFHISPM
+
+#fix 2002
+#grand total
+colnames(ef2002a)[colnames(ef2002a)=="EFRACE24"] <- "EFTOTLT"
+#total men
+colnames(ef2002a)[colnames(ef2002a)=="EFRACE15"] <- "EFTOTLM"
+# total hispanic men and women
+colnames(ef2002a)[colnames(ef2002a)=="EFRACE21"] <- "EFHISPT"
+
+#fix 2003
+#grand total
+colnames(ef2003a)[colnames(ef2003a)=="EFRACE24"] <- "EFTOTLT"
+#total men
+colnames(ef2003a)[colnames(ef2003a)=="EFRACE15"] <- "EFTOTLM"
+# total hispanic men and women
+colnames(ef2003a)[colnames(ef2003a)=="EFRACE21"] <- "EFHISPT"
+
+#fix 2004
+#grand total
+colnames(ef2004a)[colnames(ef2004a)=="EFRACE24"] <- "EFTOTLT"
+#total men
+colnames(ef2004a)[colnames(ef2004a)=="EFRACE15"] <- "EFTOTLM"
+# total hispanic men and women
+colnames(ef2004a)[colnames(ef2004a)=="EFRACE21"] <- "EFHISPT"
+
+#fix 2005
+#grand total
+colnames(ef2005a)[colnames(ef2005a)=="EFRACE24"] <- "EFTOTLT"
+#total men
+colnames(ef2005a)[colnames(ef2005a)=="EFRACE15"] <- "EFTOTLM"
+# total hispanic men and women
+colnames(ef2005a)[colnames(ef2005a)=="EFRACE21"] <- "EFHISPT"
+
+#fix 2006
+#grand total
+colnames(ef2006a)[colnames(ef2006a)=="EFRACE24"] <- "EFTOTLT"
+#total men
+colnames(ef2006a)[colnames(ef2006a)=="EFRACE15"] <- "EFTOTLM"
+# total hispanic men and women
+colnames(ef2006a)[colnames(ef2006a)=="EFRACE21"] <- "EFHISPT"
+
+#fix 2007
+#grand total
+colnames(ef2007a)[colnames(ef2007a)=="EFRACE24"] <- "EFTOTLT"
+#total men
+colnames(ef2007a)[colnames(ef2007a)=="EFRACE15"] <- "EFTOTLM"
+# total hispanic men and women
+colnames(ef2007a)[colnames(ef2007a)=="EFRACE21"] <- "EFHISPT"
+
+
 #2000-2017 (file is too big)
 EFA <- mget(ls(pattern = c("^ef\\d{4}a$")))
 
@@ -212,12 +302,13 @@ variables[nrow(variables) + 1,] = list(Table= factor("EF_A"),Variable.Number= 99
 EFA_var <- variables[variables$Table == "EF_A", 3]
 
 # Merge and and only use the columns in our variable list
+
+EFA <- lapply(EFA, function(x) x[(names(x)) %in% EFA_var])
 EFA_df <- do.call("smartbind", EFA)
-EFA_df <- EFA_df[ ,colnames(EFA_df) %in% EFA_var]
 
 
 
-#Before 2000
+######Before 2000
 survey_file <- "ef"
 capital_survey_file <- "EF"
 extra <- "_a"
@@ -306,12 +397,13 @@ for (year in 2000:2017){
            getIPEDSData(year, survey_file, capital_survey_file, extra, capital_extra))
 }
 
-IC <- mget(ls(pattern = "ic\\d{4}"))
+IC <- mget(ls(pattern = "^ic\\d{4}$"))
 
 levels(variables$Variable.Name) <- c(levels(variables$Variable.Name),"YEAR") 
 variables[nrow(variables) + 1,] = list(Table= factor("IC"),Variable.Number= 9999, Variable.Name= factor("YEAR"))
 IC_var <- variables[variables$Table == "IC", 3]
 
 # Merge and and only use the columns in our variable list
+IC <- lapply(IC, function(x) x[(names(x)) %in% IC_var])
 IC_df <- do.call("smartbind", IC)
-IC_df <- IC_df[ ,colnames(IC_df) %in% IC_var]
+
