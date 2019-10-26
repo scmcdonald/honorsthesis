e72ebe39c6df5b64b9fd91db2f798252ead1f43c
Sarah <scm81106@marymount.edu>
2019-10-26 00:43:28 -0400

    simplified geo

--- read-in-kmz.R
+++ read-in-kmz.R
@@ -1,14 +1,11 @@
 #gray.ftp.clickability.com
+#https://gavinr.com/catholic-dioceses-of-the-us-map/
+#https://www.arcgis.com/home/webmap/viewer.html?webmap=a945f185eab041378d595f6c0a7f628f
 library(sf)
-library(gdalUtilities)
-library(rgdal)
-library(maptools)
 library(ggplot2)
-library(rvest)
+library(maptools)
 
-KMZs <- list.files(path="C:/Users/Sarah McDonald/Documents/honorsthesis", pattern="*.kmz", full.names=FALSE)
-
-LonLat <- sapply(KMZs, function(x)getKMLcoordinates(kmlfile = unzip(zipfile = paste0("C:/Users/Sarah McDonald/Documents/honorsthesis/", x),  exdir = "~/honorsthesis/KML"), ignoreAltitude = TRUE))
+KML <- getKMLcoordinates(kmlfile = unzip(zipfile = "C:/Users/Sarah McDonald/Documents/honorsthesis/US_Dioceses.kmz", exdir = "~/honorsthesis/KML"), ignoreAltitude = TRUE)
 
 cath_geo <- st_read("KML/doc.kml")
 
