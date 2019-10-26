80af2d33041f581de5e5eba9b35572fbf87aadc1
Sarah <scm81106@marymount.edu>
2019-10-26 11:34:58 -0400

    more geo

--- dioceses-geo.R
+++
@@ -1,6 +0,0 @@
-library(sf)
-https://www.arcgis.com/home/item.html?id=69960b690e6e4cb3aef6fc2e5ad39456
-map_dioceses <- st_read("US_Dioceses.kmz")
-
-library(maptools)
-getKMLcoordinates(textConnection(system("unzip -p US_Dioceses.kmz")))
--- read-in-kmz.R
+++ read-in-kmz.R
@@ -4,13 +4,27 @@
 library(sf)
 library(ggplot2)
 library(maptools)
+library(dplyr)
+library(stringr)
 
 KML <- getKMLcoordinates(kmlfile = unzip(zipfile = "C:/Users/Sarah McDonald/Documents/honorsthesis/US_Dioceses.kmz", exdir = "~/honorsthesis/KML"), ignoreAltitude = TRUE)
 
 cath_geo <- st_read("KML/doc.kml")
 
+cath_geo$Description <- as.character(as.factor(cath_geo$Description))
+
+head(cath_geo$Description)
+
+#get str_extract to select more
+
+cath_geo$Description <- str_extract(cath_geo$Description, 'Diocese(.*)SUM_SUM_POP2010')
+
+cath_geo$Description <- lapply(cath_geo$Description, function(x) gsub(c("(Diocese)?<.?t(d|r)>(SUM_SUM_POP2010)?"), "", x))
+
+cath_geo$Description <- trimws(cath_geo$Description)
+colnames(cath_geo)[colnames(cath_geo) == "Description"] <- "Dioceses"
+
+
 ggplot()+
   geom_sf(data= cath_geo)
-
-
 
